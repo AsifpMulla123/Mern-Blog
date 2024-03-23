d2a57512def8ff8ad8071fa40cf25607f3c9becb
Asif <ashupmulla@gmail.com>
2024-03-23 11:32:47 +0530

    adding like functionality to the comment section

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,14 +1,15 @@
-import { Button, Textarea } from "flowbite-react";
-import React, { useEffect, useState } from "react";
+import { Alert, Button, TextInput, Textarea } from "flowbite-react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Comment from "./Comment";
 
-const CommentSection = ({ postId }) => {
+export default function CommentSection({ postId }) {
   const { currentUser } = useSelector((state) => state.user);
   const [comment, setComment] = useState("");
-  const [commentError, setCommentError] = useState("");
+  const [commentError, setCommentError] = useState(null);
   const [comments, setComments] = useState([]);
+  const navigate = useNavigate();
   console.log(comments);
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -28,7 +29,6 @@ const CommentSection = ({ postId }) => {
         }),
       });
       const data = await res.json();
-      console.log(data);
       if (res.ok) {
         setComment("");
         setCommentError(null);
@@ -38,6 +38,7 @@ const CommentSection = ({ postId }) => {
       setCommentError(error.message);
     }
   };
+
   useEffect(() => {
     const getComments = async () => {
       try {
@@ -52,18 +53,46 @@ const CommentSection = ({ postId }) => {
     };
     getComments();
   }, [postId]);
+
+  const handleLike = async (commentId) => {
+    try {
+      if (!currentUser) {
+        navigate("/sign-in");
+        return;
+      }
+      const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+        method: "PUT",
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setComments(
+          comments.map((comment) =>
+            comment._id === commentId
+              ? {
+                  ...comment,
+                  likes: data.likes,
+                  numberOfLikes: data.likes.length,
+                }
+              : comment
+          )
+        );
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
       {currentUser ? (
         <div className="flex items-center gap-1 my-5 text-gray-500 text-sm">
-          <p>Signed in as :</p>
+          <p>Signed in as:</p>
           <img
+            className="h-5 w-5 object-cover rounded-full"
             src={currentUser.profilePicture}
             alt=""
-            className="h-5 w-5 object-cover rounded-full"
           />
           <Link
-            to="/dashboard?tab=profile"
+            to={"/dashboard?tab=profile"}
             className="text-xs text-cyan-600 hover:underline"
           >
             @{currentUser.username}
@@ -71,16 +100,16 @@ const CommentSection = ({ postId }) => {
         </div>
       ) : (
         <div className="text-sm text-teal-500 my-5 flex gap-1">
-          You must signed in to comment.
-          <Link to="/sign-in" className="text-blue-500 hover:underline">
+          You must be signed in to comment.
+          <Link className="text-blue-500 hover:underline" to={"/sign-in"}>
             Sign In
           </Link>
         </div>
       )}
       {currentUser && (
         <form
-          className="border border-teal-500 rounded-md p-3"
           onSubmit={handleSubmit}
+          className="border border-teal-500 rounded-md p-3"
         >
           <Textarea
             placeholder="Add a comment..."
@@ -90,8 +119,8 @@ const CommentSection = ({ postId }) => {
             value={comment}
           />
           <div className="flex justify-between items-center mt-5">
-            <p className="text-gray-500  text-sm">
-              {200 - comment.length} characters left remaining
+            <p className="text-gray-500 text-xs">
+              {200 - comment.length} characters remaining
             </p>
             <Button outline gradientDuoTone="purpleToBlue" type="submit">
               Submit
@@ -115,12 +144,10 @@ const CommentSection = ({ postId }) => {
             </div>
           </div>
           {comments.map((comment) => (
-            <Comment key={comment._id} comment={comment} />
+            <Comment key={comment._id} comment={comment} onLike={handleLike} />
           ))}
         </>
       )}
     </div>
   );
-};
-
-export default CommentSection;
+}
