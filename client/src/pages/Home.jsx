bb43c159912997ce7508b9131f8aa27eea8c6ca1
Asif <ashupmulla@gmail.com>
2024-03-24 16:19:53 +0530

    completing home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
+import { useEffect, useState } from "react";
+import PostCard from "../components/PostCard";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch("/api/post/getposts");
+      const data = await res.json();
+      setPosts(data.posts);
+    };
+    fetchPosts();
+  }, []);
 
-export default Home
+  return (
+    <div>
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
+        <h1 className="text-3xl font-bold lg:text-6xl">
+          Are you ready to take your knowledge to the next level?
+        </h1>
+        <p className="text-gray-500 text-sm sm:text-md">
+          Stay informed with the latest in tech and finance! Our blog delivers
+          cutting edge news and expert insights.Subscribe today for a smarter
+          tomorrow!
+        </p>
+        <Link
+          to="/search"
+          className="text-xs sm:text-sm text-teal-500 font-bold hover:underline"
+        >
+          View all posts
+        </Link>
+      </div>
+      <div className="p-3 bg-amber-100 dark:bg-slate-700">
+        <CallToAction />
+      </div>
+      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
+        {posts && posts.length > 0 && (
+          <div className="flex flex-col gap-6">
+            <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
+            <div className="flex flex-wrap gap-4">
+              {posts.map((post) => (
+                <PostCard key={post._id} post={post} />
+              ))}
+            </div>
+            <Link
+              to="/search"
+              className="text-lg text-teal-500 hover:underline text-center"
+            >
+              View all posts
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
